3222b58e57369108eab22528f2989fa76212b491
CmEinsig <cmneiderer@gmail.com>
2023-05-02 12:48:06 -0400

    added route in even for updating and deleting

--- controllers/api/eventRoutes.js
+++ controllers/api/eventRoutes.js
@@ -20,7 +20,43 @@ router.post('/', passwordAuth, async (req, res) => {
 });
 
 //Route for updating an event
+router.put("/:event", passwordAuth, async (req, res) => {
+  try {
+      const newEvent = await User.update(
+          {
+              eventName: req.body.eventName,
+              eventLocation: req.body.eventLocation, 
+              eventIndustry: req.body.eventIndustry,
+              aboutEvent: req.body.aboutEvent 
+          },
+          {
+              where: {
+                  id: req.params.event
+              }
+          }
+      )
+      res.status(200).json(updatedEvent)
+  } catch (err) {
+      res.status(400).json(err)
+  }
+})
 
 //Route for deleting an event
+router.delete("/:event", passwordAuth, async (req, res) => {
+  try {
+      const eventData = await Event.destroy({
+          where: {
+              id: req.params.event
+          }
+      })
+      if (!eventData) {
+          res.status(404).json({ message: "No event found" })
+          return
+      }
+      res.status(200).json(eventData)
+  } catch (err) {
+      res.status(500).json(err)
+  }
+})
 
 module.exports = router;
