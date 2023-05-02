2d5dfff3ec3d3c0be018b513fa9be951d35229c8
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-05-02 11:29:22 -0400

    updated commentRoutes.js to remove get and replace with delete,started working on openAI.js

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,23 +1,34 @@
 const router = require('express').Router();
 const { Comment } = require('../../models/');
-const withAuth = require('../../utils/auth');
+const withAuth = require('../../utils/passwordAuth');
 
-// Route for getting all comments for a post
-router.get('/', withAuth, async (req, res) => {
+// Route for deleting a comment
+router.delete('/:id', withAuth, async (req, res) => {
   try {
-    // Get all comment data, including the user who made the comment
-    const commentData = await Comment.findAll({
-      include: [User],
-    });
-    // Serialize the comment data to an array of plain objects
-    const comments = commentData.map((comment) => comment.get({ plain: true }));
+    // Find the comment to be deleted by its id
+    const commentData = await Comment.findByPk(req.params.id);
 
-    // Render the single-post handlebars template with the comments and logged in status
-    res.render('single-post', { comments, loggedIn: req.session.loggedIn });
+    // Check if the comment exists
+    if (!commentData) {
+      res.status(404).json({ message: 'Comment not found' });
+      return;
+    }
+
+    // Check if the current user is the creator of the comment
+    if (commentData.user_id !== req.session.user_id) {
+      res.status(403).json({ message: 'You are not authorized to delete this comment' });
+      return;
+    }
+
+    // Delete the comment
+    await commentData.destroy();
+
+    res.status(200).json({ message: 'Comment successfully deleted' });
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
 
 // Route for creating a new comment
 router.post('/', withAuth, async (req, res) => {
---
+++ public/js/openAI.js
@@ -0,0 +1,44 @@
+// Get the HTML elements for the about me section
+const aboutMe = document.getElementById("about-me");
+const aboutMeInput = document.getElementById("about-me-input");
+const aboutMeBtn = document.getElementById("about-me-btn");
+
+// Set the API key for ChatGPT
+const apiKey = "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";
+
+// Check if the user wants to use ChatGPT to generate their about me section
+aboutMeBtn.addEventListener("click", () => {
+  const generateAboutMe = confirm("Would you like to use ChatGPT to generate your About Me section?");
+  if (generateAboutMe) {
+    // Check if the user has a public LinkedIn profile
+    if (!user.linkedinURL || !user.linkedinURL.includes("linkedin.com/in/")) {
+      alert("Please make sure you have a public LinkedIn profile before using ChatGPT to generate your About Me section.");
+      return;
+    }
+    // Use ChatGPT to generate the about me section
+    fetch("https://api.openai.com/v1/engine/davinci-codex/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({
+        "prompt": `Based on ${user.linkedinURL}, write an about me for a networking profile, no more than 2 paragraphs.`,
+        "temperature": 0.7,
+        "max_tokens": 1024
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      const aboutMeText = data.choices[0].text.trim();
+      // Append the generated about me text to the HTML element
+      aboutMe.innerHTML = `<hr><h2 class="subtitle">About Me</h2><p>${aboutMeText}</p>`;
+    })
+    .catch(error => console.error(error));
+  }
+});
+
+// Allow the user to edit the about me section
+aboutMeInput.addEventListener("input", () => {
+  aboutMe.innerHTML = `<hr><h2 class="subtitle">About Me</h2><p>${aboutMeInput.value}</p>`;
+});
