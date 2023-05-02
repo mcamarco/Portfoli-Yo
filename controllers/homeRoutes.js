54d2f63b5f1351056fcc082ce5077ef143388aab
Elva Rothman <erothman@tulane.edu>
2023-05-02 13:56:51 -0400

    added formatDate helper function to utils

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router()
-const { User, Event, Comment } = require("../../models")
+const { User, Event, Comment } = require("../models")
 const passwordAuth = require("../utils/passwordAuth")
 
 //Get request to render homepage
